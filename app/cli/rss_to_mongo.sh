8d5d0399627108012d9afe6e21896903877cc5f5
Freddie FUJIWARA <github@ze.gs>
2012-02-26 18:48:00 +0900

    bug fix

--- app/cli/rss_to_mongo.sh
+++ app/cli/rss_to_mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /var/lib/jenkins/.nvm/nvm.sh
 nvm use v0.6.11
-/var/lib/jenkins/jobs/absolutely_must_not_fail/workspace/node_modules/coffee-script/bin/coffee app/cli/crawl.coffee
+/var/lib/jenkins/jobs/absolutely_must_not_fail/workspace/node_modules/coffee-script/bin/coffee /var/lib/jenkins/jobs/absolutely_must_not_fail/workspace/app/cli/crawl.coffee
